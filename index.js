4bbc1081e2c18ac65443d25211ee35a751105f5e
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-13 20:50:48 -0800

    add event listener to search button

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
 const tasteDiveUrl = "https://tastedive.com/api/similar";
 
-
-function requestFromTasteKid(searchVal, callback){
-  const query = {
-    q:`${searchVal}`,
-    type:"books",
-    info: 1,
-    k:"296844-TylerSti-3Q7R0SNX",
-    dataType: "jsonp",
-  };
-
-  $.getJSON(tasteDiveUrl, query, callback);
-  console.log(`${$("#start-page-search").val()}`);
-}
-
 function handleSearchButton(){
+  console.log("start of handleSearchButton working");
   $(".js-search-button").on("click", function(){
-    let searchInputValue= this.prev().val();
 
-    requestFromTasteKid(searchInputValue, callback);
+    let searchInputValue= $(this).prev().val();
+
+    console.log(searchInputValue);
+
+    requestFromTasteKid(searchInputValue, function(obj){
+      console.log(obj.similar.info.results);
+      if(obj.similar.length() === 0){
+        $(".js-main").append(`<p>Sorry, there are no results for ${searchInputValue}</p>`);
+      }else{
+        const results = $(data.similar.info.results).map(function(result){
+          return result;
+        });
+
+        $(".js-main").append(
+          `<aside role="region">
+            <p>${results}</p>
+          </aside>`
+        );
+      }
+    });
   });
+  console.log("end of handleSearchButton working");
 }
+
 
 function renderBookSuggestions(){
   //append results to html
@@ -42,6 +49,19 @@ function renderPriceComparisons(){
   //update DOM with prices comparisons
 }
 
+function requestFromTasteKid(searchVal, callback){
+  const query = {
+    q:`${searchVal}`,
+    type:"books",
+    info: 1,
+    k:"296844-TylerSti-3Q7R0SNX",
+    dataType: "jsonp",
+  };
+
+  $.getJSON(tasteDiveUrl, query, callback);
+  console.log(`${searchVal}`);
+}
 
 
-$(requestFromTasteKid);
+
+$(handleSearchButton);
