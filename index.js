8bd57cf7cbeb5ebff5e21b9d9ab933fb4edd9875
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-13 18:33:57 -0800

    add function stubs

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-const tastDiveUrl = "https://tastedive.com/api/similar";
+const tasteDiveUrl = "https://tastedive.com/api/similar";
 
 
-function requestFromTasteKid(url, callback){
+function requestFromTasteKid(searchVal, callback){
   const query = {
-    q:`${$("#book-search-query").val()}`,
+    q:`${searchVal}`,
     type:"books",
     info: 1,
     k:"296844-TylerSti-3Q7R0SNX",
     dataType: "jsonp",
   };
 
-  $.getJSON(tastDiveUrl, query, callback);
+  $.getJSON(tasteDiveUrl, query, callback);
   console.log(`${$("#start-page-search").val()}`);
+}
+
+function handleSearchButton(){
+  $(".js-search-button").on("click", function(){
+    let searchInputValue= this.prev().val();
+
+    requestFromTasteKid(searchInputValue, callback);
+  });
+}
+
+function renderBookSuggestions(){
+  //append results to html
+}
+
+function handleNewSearch(){
+  //when user inputs new search update DOM
+}
+
+function handleSuggestionClick(){
+  //book suggestion is clicked
+}
+
+function requestForPrices(){
+  //when book suggestion is clicked make call to Direct Textbook api
+}
+
+function renderPriceComparisons(){
+  //update DOM with prices comparisons
 }
 
 
