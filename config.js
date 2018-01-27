b2eb806ac696b8f751e7ed0993584686eb5bcf7c
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-26 21:35:32 -0800

    Add git-secret

---
+++ config.js
@@ -0,0 +1,6 @@
+var keys={
+  tasteDive: "296844-TylerSti-CJ8GIDVA",
+  googleBooks: "AIzaSyAorrsA86m43Oyscx1iw2cJNeRuDZ5en8k",
+  amazonWebServicesAccessKeyId: "AKIAJ2EHGYIPJZK6Z5JA",
+  secretKey: "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"
+}
