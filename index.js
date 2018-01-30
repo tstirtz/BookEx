5b73b966ef122742ac1c2b30da38f71653e28e41
Tyler Stirtz <tyler.stirtz@gmail.com>
2018-01-30 13:34:46 -0800

    Fix styling that was missed in last branch

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ function requestToAmazonForUsedPrices(pricesUrl){
         let estimatedShipping = shippingDates[index]['children'][0]['childNodes'][2]['data'];
 
 
-        $(`.${index}`).append(`<div class= "col-2"><strong>Shipping Date:</strong><br>${estimatedShipping}</div>`);
+        $(`.${index}`).append(`<div class= " shipping-date col-2"><strong>Shipping Date:</strong><br>${estimatedShipping}</div>`);
       });
 
       //retrieve seller name
@@ -240,7 +240,7 @@ function requestToAmazonForUsedPrices(pricesUrl){
         let offerIdValue = offerIds[index]['value'];
 
 
-        $(`.${index}`).append(`<div class="purchase-button col-2"><a href = "#" type="button" class= "js-purchase-book-${index}" id="${offerIdValue}">Buy</button></a></div>`);
+        $(`.${index}`).append(`<div class="purchase-button col-2"><a href = "#" type="button" class= "js-purchase-book-${index}" id="${offerIdValue}">Buy From Amazon</button></a></div>`);
 
         handleBuyButton(index);
       });
